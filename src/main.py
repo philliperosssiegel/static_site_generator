320c049faea8fa6a4ead3543cda1a4147743efd0
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2025-12-23 16:03:40 -0500

    Refactored to be more in-line with the solution files

--- src/main.py
+++ src/main.py
@@ -1,13 +1,7 @@
 from textnode import TextNode, TextType
 
-def main():
-    """
-    The main function of the script.
-    Contains the primary logic to be executed.
-    """
-    print("hello world")
-    
-    text_node = TextNode("This is some anchor text", TextType("link"), "https://www.boot.dev")
+def main():  
+    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(text_node)
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,18 +1,12 @@
 from enum import Enum
 
-class Bender(Enum):
-    AIR_BENDER = "air"
-    WATER_BENDER = "water"
-    EARTH_BENDER = "earth"
-    FIRE_BENDER = "fire"
-
 class TextType(Enum):
-    text = "text"
-    bold = "bold"
-    italic = "italic"
-    code = "code"
-    link = "link"
-    image = "image"
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode():
     def __init__(self, text: str, text_type: TextType , url=None):
@@ -20,13 +14,8 @@ class TextNode():
         self.text_type = text_type
         self.url = url
     
-    def __eq__(self, text_node: "TextNode"):
-        return self.text == text_node.text and self.text_type == text_node.text_type and self.url == text_node.url
-        # # for prop in self:
-        # #     if self[prop] != text_node[prop]:
-        # #         return False
-        # # return True
-        # return False
+    def __eq__(self, other: "TextNode"):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
