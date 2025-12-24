97f00cf368f0832f6571f5d909c43283467da6c9
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2025-12-24 12:41:47 -0500

    Completed CH3 L4

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,5 @@
+import re
+
 from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -21,3 +23,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(text, text_type))
     
     return new_nodes
+
+def extract_markdown_images(text: str):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text: str):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
