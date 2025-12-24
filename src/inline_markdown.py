efd9207f323ee30f780440552365e49c9c0d7a19
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2025-12-24 12:24:52 -0500

    Completed CH3 L1

---
+++ src/inline_markdown.py
@@ -0,0 +1,23 @@
+from textnode import TextType, TextNode
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        split_text = node.text.split(delimiter)
+
+        if len(split_text) % 2 == 0:
+            raise ValueError(f"Invalid text, index-parity failure for delimiter={delimiter}")
+
+        for i, text in enumerate(split_text):
+            if text == "":
+                continue
+            elif i % 2 == 0: #even --> not inline text
+                new_nodes.append(TextNode(text, TextType.TEXT))
+            else: #odd --> inline text
+                new_nodes.append(TextNode(text, text_type))
+    
+    return new_nodes
