62bf2dc67bee4b3a994e64bace34643a411c2690
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2025-12-24 10:56:16 -0500

    Completed CH2 L6

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -19,4 +20,18 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-    
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type.value:
+        case "text":
+            return LeafNode(None, text_node.text)
+        case "bold":
+            return LeafNode("b", text_node.text)
+        case "italic":
+            return LeafNode("i", text_node.text)
+        case "code":
+            return LeafNode("code", text_node.text)
+        case "link":
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case "image":
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
