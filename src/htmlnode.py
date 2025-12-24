94d96cc7c0309b14b58e3eb59ac68e15a4cfa6ce
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2025-12-24 10:11:45 -0500

    Refactored to be more in-line with the solution files

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,15 +6,18 @@ class HTMLNode:
         self.props = props
     
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
     def to_html(self):
         raise NotImplementedError("to_html() method not implemented for HTMLNode class")
     
     def props_to_html(self):
-        if not self.props:
-            return f"""{{\n\t"href": "{href_value}",\n\t"target": "{target_value}",\n}}"""
-        return None
+        if self.props is None:
+            return ""
+        props_str = ""
+        for k, v in self.props.items():
+            props_str = f'{props_str} {k}="{v}"'
+        return props_str
 
 class LeafNode(HTMLNode):
     def __init__(self, tag: str, value: str, props: dict = None):
@@ -23,12 +26,6 @@ class LeafNode(HTMLNode):
     def to_html(self):
         if self.value is None:
             raise ValueError("LeafNode missing required value parameter value")
-        if not self.tag:
-            return self.value
-        if self.props:
-            props_str = ""
-            for k, v in self.props.items():
-                props_str = f'{props_str} {k}="{v}"'
-            
-            return f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
-        return f"<{self.tag}>{self.value}</{self.tag}>"
+        if self.tag is None:
+            return self.value 
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
