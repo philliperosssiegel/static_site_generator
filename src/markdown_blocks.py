64e44dba5d09d2122ee6346a965fbec299003f33
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2025-12-27 15:29:32 -0500

    Completed CH4 L2

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,65 @@
+from enum import Enum
+from functools import reduce
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph",
+    HEADING = "heading",
+    CODE = "code",
+    QUOTE = "quote",
+    UNORDERED_LIST = "unordered_list",
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(markdown_block: str) -> BlockType:
+    reduce_and = lambda x, y: x and y
+    try:
+
+        if (reduce(reduce_and, [char=="#" for char in markdown_block.split()[0]])):
+            return BlockType.HEADING
+        elif (markdown_block[:3] == "```" and markdown_block[-3:] == "```"):
+            return BlockType.CODE
+        elif (reduce(reduce_and, [line[0]==">" for line in markdown_block.split("\n")])):
+            return BlockType.QUOTE
+        elif (reduce(reduce_and, [line[:2]=="- " for line in markdown_block.split("\n")])):
+            return BlockType.UNORDERED_LIST
+        # elif (reduce(reduce_and, [line[0].isdigit() and line[1:3]==". " for line in markdown_block.split("\n")])):
+        #     for char in [line[0] for line in markdown_block.split("\n")]:
+        previous_num = None
+        is_ordered = True
+
+        for line in markdown_block.split("\n"):
+            # require ". " to even consider it ordered
+            if ". " not in line:
+                is_ordered = False
+                break
+
+            num_section = line.split(". ")[0]
+
+            if num_section.isdigit():
+                num_section = int(num_section)
+                if previous_num is None:
+                    if num_section == 1:
+                        previous_num = num_section
+                    else:
+                        is_ordered = False
+                        break
+                else:
+                    if num_section == previous_num + 1:
+                        previous_num = num_section
+                    else:
+                        is_ordered = False
+                        break
+            else:
+                is_ordered = False
+                break
+
+        if is_ordered:
+            return BlockType.ORDERED_LIST
+
+        return BlockType.PARAGRAPH
+
+    except Exception:
+        return BlockType.PARAGRAPH
+
 def markdown_to_blocks(markdown: str):
     markdown_blocks = []
     for block in markdown.split("\n\n"): #presumed that all input markdown is "well-formed" --> double newline block separation
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -45,7 +45,53 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+    def test_get_markdown_block_type__paragraph(self):
+        mk = """
+general markdown
+line
+line
+asdvkmglkgmnd fm fdmf f kmm fmd fm fdsm ;fsdmf 121948 8 1848193148 38 1831 
+"""
+        self.assertEqual(block_to_block_type(mk), BlockType.PARAGRAPH)
 
+    def test_get_markdown_block_type__headers(self):
+        mk = """
+# general markdown
+## line
+### line
+###### asdvkmglkgmnd fm fdmf f kmm fmd fm fdsm ;fsdmf 121948 8 1848193148 38 1831
+#
+### 
+"""
+        self.assertEqual(block_to_block_type(mk), BlockType.HEADING)
+    
+    # def test_get_markdown_block_type__code(self):
+    #     mk = """ ``` fdlmfk mdf m13r13prmrp2 mp1mr m ``` """
+    #     self.assertEqual(block_to_block_type(mk), BlockType.CODE)
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_order_list_block_checks(self):
+        block = "1. one\n2. two\n3. three"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "2. one\n3. two"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+        block = "1. one\n3. three"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+        block = "1. one\nnot list\n2. two"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
