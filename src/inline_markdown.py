fbe2ed18466078f1c46ede8a6417a5c42d9e25b6
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2025-12-26 22:06:42 -0500

    Completed CH4 L1

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -2,6 +2,15 @@ import re
 
 from textnode import TextType, TextNode
 
+def markdown_to_blocks(markdown: str):
+    markdown_blocks = []
+    for block in markdown.split("\n\n"): #presumed that all input markdown is "well-formed" --> double newline block separation
+        clean_block = block.strip()
+        if not (clean_block in ["", "\n"]):
+            markdown_blocks.append(clean_block)
+    
+    return markdown_blocks
+
 def text_to_textnodes(text):
     nodes = [TextNode(text)]
     delimiter_dict = {
