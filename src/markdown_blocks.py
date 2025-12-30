9c922641e4cec3a30adce95465241da70014b91b
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2025-12-30 17:26:16 -0500

    Cleaned up markdown_blocks.py file

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -16,22 +16,6 @@ class BlockType(Enum):
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
 
-    # html_nodes = []
-    # for block in blocks:
-    #     block_type = block_to_block_type(block)
-        
-    #     if block_type != TextType.CODE:
-    #         parent_text_node = TextNode("", block_type)
-    #         parent_html_node = text_node_to_html_node(parent_text_node)
-    #         parent_html_node.children = text_to_textnodes(block)
-    #         html_nodes.append(parent_html_node)
-    #     else:
-    #         parent_text_node = TextNode(block, block_type)
-    #         parent_html_node = text_node_to_html_node(parent_text_node)
-    #         html_nodes.append(parent_html_node)
-
-    # return htmlNode(tag="div", value=None, children=html_nodes)
-
     html_nodes = []
     for block in blocks:
         html_nodes.append(create_block_html_parent_node(block))    
@@ -53,43 +37,34 @@ def create_block_html_parent_node(block_text: str):
             inline_text = block_text.lstrip("#").lstrip(" ")
         else: #QUOTE
             parent_tag = "blockquote"
-            # inline_text = ""
-            # for line in block_text.split("\n"):
-            #     inline_text += f"\n{line.lstrip('>').lstrip()}"
             lines = block_text.split("\n")
+            
             for line in lines:
                 if not line.startswith(">"):
                     raise ValueError("Invalid quote block")
+            
             stripped = [line.lstrip(">").lstrip() for line in lines]
             inline_text = " ".join(stripped)
-            # inline_text = block_text.lstrip(">").lstrip()
+
         parent_node = ParentNode(tag=parent_tag, children=text_to_children(inline_text))
     elif block_type == BlockType.CODE:
         if not block_text.startswith("```") or not block_text.endswith("```"):
             raise ValueError("Invalid code block")
-        # inline_text = block_text.lstrip("`").rstrip("`")
-        # inline_text = ""
-
-        # for line in block_text.split("\n"):
-        #     if line != "```":
-        #         # inline_text += f"\n{line}"
-        #         lines.append(line)
-
-        # lines = block_text.split("\n")
-        # inline_text = "\n".join(lines[1:-1]) + "\n" #drop ``` first and last lines
-
+        
         inline_text = block_text[4:-3]
         raw_text_node = TextNode(inline_text, TextType.TEXT)
         child_node = text_node_to_html_node(raw_text_node)
+
         parent_node = ParentNode(tag="pre", children=[ParentNode(tag="code", children=[child_node])])
     elif block_type in [BlockType.OLIST, BlockType.ULIST]:
         if block_type == BlockType.OLIST:
             parent_tag = "ol"
         else: #ULIST
             parent_tag = "ul"
+        
         lines = block_text.split("\n")
         parent_node = ParentNode(tag=parent_tag, children=[]) 
-        # ul_node = HTMLNode(tag="ul", children=[])
+        
         for line in lines:
             if block_type == BlockType.OLIST:
                 line_text = line.split(".", 1)[1].lstrip()
@@ -104,6 +79,7 @@ def get_heading_type(text):
     heading_level = len(text) - len(text.lstrip("#"))
     if heading_level + 1 >= len(text):
         raise ValueError(f"Invalid heading level: {heading_level}") 
+    
     return heading_level
 
 def text_to_children(text):
