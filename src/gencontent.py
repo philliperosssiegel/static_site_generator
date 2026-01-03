585aec0580eaef111eeea267474ad91241677102
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2026-01-03 11:01:55 -0500

    Changes for GitHub Deployment (CH5 L5)

--- src/gencontent.py
+++ src/gencontent.py
@@ -13,7 +13,7 @@ def write_file(content: str, file_path: str):
     with open(file_path, 'w') as f:
         f.write(content)
 
-def generate_page(from_path: str, dest_path: str, template_path: str):
+def generate_page(from_path: str, dest_path: str, template_path: str, basepath: str):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     source_file, template_file = read_file(from_path), read_file(template_path)
 
@@ -21,26 +21,22 @@ def generate_page(from_path: str, dest_path: str, template_path: str):
 
     title = extract_title(source_file)
 
-    prepped_html = template_file.replace("{{ Title }}", title).replace("{{ Content }}", source_file_html)
+    prepped_html = template_file.replace("{{ Title }}", title)
+    prepped_html = prepped_html.replace("{{ Content }}", source_file_html)
+    prepped_html = prepped_html.replace('href="/', f'href="{basepath}')
+    prepped_html = prepped_html.replace('src="/', f'src="{basepath}')
 
     make_dir_if_not_exists(os.path.dirname(dest_path))
     write_file(prepped_html, dest_path)
 
-def generate_pages_recursive(dir_path_content: str, dest_dir_path: str, template_path: str):
-    print("generate_pages_recursive()...")
-    print()
-    print(f"dir_path_content: {dir_path_content}, dest_dir_path: {dest_dir_path}, template_path: {template_path}")
-    print()
-
+def generate_pages_recursive(dir_path_content: str, dest_dir_path: str, template_path: str, basepath: str):
     for item in os.listdir(dir_path_content):
         full_item_path = os.path.join(dir_path_content, item)
         if os.path.isfile(full_item_path):
             if get_filetype(full_item_path) == "md":
                 filename_no_ext = get_filename(item).split(".")[0]
                 new_filename = f"{filename_no_ext}.html"
-                generate_page(full_item_path, os.path.join(dest_dir_path, new_filename), template_path)
+                generate_page(full_item_path, os.path.join(dest_dir_path, new_filename), template_path, basepath)
         else: #item is a directory
-            print(f"item={full_item_path}")
-            print(os.path.isfile(full_item_path))
             make_dir_if_not_exists(os.path.join(dest_dir_path, item))
-            generate_pages_recursive(full_item_path, os.path.join(dest_dir_path, item), template_path)
+            generate_pages_recursive(full_item_path, os.path.join(dest_dir_path, item), template_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import os
+import sys
 
 from textnode import TextNode, TextType
 from copystatic import recursive_directory_copy
 from gencontent import generate_pages_recursive
 
-def main():  
-    recursive_directory_copy(os.path.abspath("static"), os.path.abspath("public"))
-    generate_pages_recursive(os.path.abspath("content"), os.path.abspath("public"), os.path.abspath("template.html"))
+def main():
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    recursive_directory_copy(os.path.abspath("static"), os.path.abspath("docs"))
+    generate_pages_recursive(os.path.abspath("content"), os.path.abspath("docs"), os.path.abspath("template.html"), basepath)    
 
 if __name__ == "__main__":
     main()
