b69f5b487373cd504f750735e383f81abb9c840b
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2026-01-02 20:00:10 -0500

    Changed filename for generator to copystatic to align with the solution files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 
 from textnode import TextNode, TextType
-from generator import recursive_directory_copy
+from copystatic import recursive_directory_copy
 
 def main():  
     recursive_directory_copy(os.path.abspath("static"), os.path.abspath("public"))
